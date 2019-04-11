70717dc8d962ccb97db1c989f9b74c3a7660c142
Ameet Dhas <dhas.ameet@gmail.com>
2019-04-11 22:10:28 +0530

    ArrayList.java

---
+++ MapReduce/src/ArrayList.java
@@ -0,0 +1,39 @@
+package utils.csv;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ArrayListTextWritable extends ArrayList<Text> implements Writable {
+    private static final long serialVersionUID = -6737762624115237320L;
+    
+    @Override
+    public void write(DataOutput dataoutput) throws IOException {
+        dataoutput.writeInt(this.size());
+        for (Text element : this) {
+            element.write(dataoutput);
+        }
+    }
+    
+    @Override
+    public void readFields(DataInput datainput) throws IOException {
+        this.clear();
+        int count = datainput.readInt();
+        for (int i = 0; i < count; i++) {
+            try {
+                Text obj = Text.class.newInstance();
+                obj.readFields(datainput);
+                this.add(obj);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
