b180c1c677707fe9ebdab0e76aa9e8c56ba145b1
Ameet Dhas <dhas.ameet@gmail.com>
2020-04-08 15:09:44 +0530

    package-info.java

---
+++ MapReduce/java/mapreduce/server/tasktracker/package-info.java
@@ -0,0 +1,5 @@
+@InterfaceAudience.Private
+@InterfaceStability.Unstable
+package org.apache.hadoop.mapreduce.server.tasktracker;
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
