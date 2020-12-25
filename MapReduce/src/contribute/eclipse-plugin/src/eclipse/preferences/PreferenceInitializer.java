6c4037b7d4003ff0e4b012c6f5061b44a4aa7c9e
Ameet <dhas.ameet@gmail.com>
2020-12-25 19:06:30 +0530

    PreferenceInitializer.java

---
+++ MapReduce/src/contribute/eclipse-plugin/src/eclipse/preferences/PreferenceInitializer.java
@@ -0,0 +1,15 @@
+package org.apache.hadoop.eclipse.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+  /* @inheritDoc */
+  @Override
+  public void initializeDefaultPreferences() {
+  }
+
+}
